538c2e0ba9664d4a844f733130d6a0769c76408a
justincdavis <davis.justin@mssm.org>
2024-02-23 13:29:41 -0700

    Allow import of run cli from cli

---
+++ src/shiftdetector/cli/__init__.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2024 Justin Davis (davisjustin302@gmail.com)
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+Module for command line interface functions.
+
+This module contains the functions for the command line interface.
+These functions when imported directly do not utilize the same
+style as the command line. Instead, you pass the argumnets as
+the objects themselves.
+
+Functions
+---------
+run
+    Run a model on a video file.
+run_cli
+    Run the model on a video from the command line.
+    Parses arguments from sys.argv.
+"""
+from __future__ import annotations
+
+from .run import run, run_cli
+
+__all__ = ["run", "run_cli"]
